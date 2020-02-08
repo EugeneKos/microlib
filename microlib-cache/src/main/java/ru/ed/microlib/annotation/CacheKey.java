fc35bcd56cce584b4d7f8a643adea64975079241
EugeneKos <Kosinov4444@yandex.ru>
2020-02-08 21:14:43 +0300

    custom-cache: Start of working on custom cache.

---
+++ microlib-cache/src/main/java/ru/ed/microlib/annotation/CacheKey.java
@@ -0,0 +1,8 @@
+package ru.ed.microlib.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CacheKey {
+}
---
+++ microlib-cache/src/main/java/ru/ed/microlib/annotation/Cacheable.java
@@ -0,0 +1,9 @@
+package ru.ed.microlib.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Cacheable {
+    int timeToLive();
+}
