5d21c3f2318a3b40e01be48c960510b1bc0ec3cd
EugeneKos <kosinov4444@yandex.ru>
2020-04-06 22:08:55 +0300

    rest-client: Rest client test via UrlConnection.

---
+++ microlib-rest-client/src/main/java/ru/ed/microlib/RestClientApp.java
@@ -0,0 +1,49 @@
+package ru.ed.microlib;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class RestClientApp {
+    public static void main(String[] args) {
+        //for (int i = 0; i < 10; i++) {
+            new Thread(new Connection()).start();
+        //}
+    }
+
+    private static class Connection implements Runnable{
+        public void run() {
+            //for (int i = 0; i < 100; i++) {
+                connect();
+            //}
+        }
+    }
+
+    private static void connect(){
+        try {
+            URL url = new URL("http://192.168.0.106:8080/app-market-web/auth");
+
+            URLConnection urlConnection = url.openConnection();
+
+            urlConnection.setRequestProperty("Accept-Charset", "utf-8");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+
+            urlConnection.setDoOutput(true);
+            OutputStream outputStream = urlConnection.getOutputStream();
+            String body = "{\"username\":\"baran\", \"password\":\"baran\"}";
+            outputStream.write(body.getBytes("utf-8"));
+
+            InputStream inputStream = urlConnection.getInputStream();
+
+            byte[] buffer = new byte[255];
+
+            while (inputStream.read(buffer) != -1){
+                System.out.println(new String(buffer));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
